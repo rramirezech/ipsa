b85fb86835bc04176895a6a468feda3a504580ad
rramirezech <rramirezech@hotmail.com>
2016-01-24 23:29:11 -0600

    Creacion de clases para manejo de usuarios

---
+++ src/java/paquetes/CatPer.java
@@ -0,0 +1,117 @@
+
+package paquetes;
+
+import java.io.Serializable;
+
+public class CatPer implements Serializable{
+    
+    private String id_per, nombres, apellidos, direccion, telefono, celular, dui, nit, isss, id_cargo, cod_usu;
+
+    public CatPer() {
+    }
+
+    public CatPer(String id_per, String nombres, String apellidos, String direccion, String telefono, String celular, String dui, String nit, String isss, String id_cargo, String cod_usu) {
+        this.id_per = id_per;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.celular = celular;
+        this.dui = dui;
+        this.nit = nit;
+        this.isss = isss;
+        this.id_cargo = id_cargo;
+        this.cod_usu = cod_usu;
+    }
+
+    public String getId_per() {
+        return id_per;
+    }
+
+    public void setId_per(String id_per) {
+        this.id_per = id_per;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getIsss() {
+        return isss;
+    }
+
+    public void setIsss(String isss) {
+        this.isss = isss;
+    }
+
+    public String getId_cargo() {
+        return id_cargo;
+    }
+
+    public void setId_cargo(String id_cargo) {
+        this.id_cargo = id_cargo;
+    }
+
+    public String getCod_usu() {
+        return cod_usu;
+    }
+
+    public void setCod_usu(String cod_usu) {
+        this.cod_usu = cod_usu;
+    }
+
+    
+        
+}
